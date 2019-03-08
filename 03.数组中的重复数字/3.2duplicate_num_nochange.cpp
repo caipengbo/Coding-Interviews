d41a68e242164584378f4d84052622f81c4113a5
caipengbo <caipengbo@outlook.com>
2019-03-08 09:21:49 +0800

    finished question 3

--- 03.数组中的重复数字/3.2duplicate_num_nochange.cpp
+++ 03.数组中的重复数字/3.2duplicate_num_nochange.cpp
@@ -5,34 +5,30 @@ class Solution {
 public:
 	// n+1 长度的数组在 1 —— n 范围内，一定存在重复数字，找出任意一个重复数字，不能修改数组！
 	int duplicate(int numbers[], int length) {
-		if (length < 1) {
+		if (numbers == nullptr || length <= 0) {
 			return -1;
-		}
-		if (length == 1) {
-			return 1;
 		}
 		int start = 1;
 		int end = length - 1;
 		int mid;
-		int num;
-		while (start < end) {
+		int count;
+		while (start <= end) {
 			mid = (end + start) / 2;
-			cout << start << "---" << mid << "---"<< end << endl;
-			num = count_num(start, mid, numbers, length);
-			if (num > mid - start + 1) {
-				if (mid == start) {
-					return start;
-				} else {
-					end = mid;	
-				}
+			count = count_num(start, mid, numbers, length);
+			if (count > mid - start + 1) {
+				end = mid - 1 ;
 			} else {
-				start = mid;
+				start = mid + 1;
 			}
+			cout << start << "---" << mid << "---" << end << endl;
 		}
-		return -1;
+		return mid;
 	}
 
 	int count_num(int start, int end, int numbers[], int length) {
+		if (numbers == nullptr) {
+			return 0;
+		}
 		int num = 0;
 		for (int i = 0; i < length; i++) {
 			if (numbers[i] >= start && numbers[i] <= end)
@@ -44,8 +40,6 @@ public:
 int main() 
 {
 	Solution s;
-	int numbers[] = { 2, 3, 5, 4, 6, 2, 6, 7};
+	int numbers[] = { 1, 3, 2, 4, 5, 7, 6, 7};
 	cout << s.duplicate(numbers, 8);
-
-
 }
