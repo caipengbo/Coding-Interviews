25a0362315e0515a35daec17059fec410531bd2f
Myth <caipengbo@outlook.com>
2019-07-13 11:24:13 +0800

    63. 股票的最大利润

---
+++ src/problem/P60DicesProbability.java
@@ -0,0 +1,10 @@
+package problem;
+
+/**
+ * Title: n 个骰子的点数
+ * Desc: 把 n 个骰子扔在地上，所有骰子朝上一面的点数之和为 s，输入 n，打印出 s 的所有可能的值出现的概率
+ * Created by Myth on 7/12/2019
+ */
+public class P60DicesProbability {
+    // TODO: 动态规划
+}
---
+++ src/problem/P63MaxProfit.java
@@ -0,0 +1,37 @@
+package problem;
+
+/**
+ * Title: P63. 股票的最大利润
+ * Desc: 把股票的价格按照时间先后顺序存储在数组中，请问买卖该股票 1 次可以获得的最大利润是多少？
+ * 例如：一只股票在某时间节点的价格是{9,11,8,5,7,12,16,14}，在价格为5 的时候买入并在价格为16 的时候卖出，最大利润为11。
+ * Created by Myth on 7/13/2019
+ */
+public class P63MaxProfit {
+    // 扫描一遍，记住当前位置之前的元素最小值
+    public int MaxDiff(int[] numbers) {
+        if (numbers == null) return -1;
+        int len = numbers.length;
+        if (len < 2) return -1;
+        int profit = Integer.MIN_VALUE;
+        int min = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if ((numbers[i]-min) > profit) profit = numbers[i]-min;
+            if (numbers[i] < min) min = numbers[i];
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        P63MaxProfit p63 = new P63MaxProfit();
+        int[] arr1 = {9,11,8,5,7,12,16,14};
+        int[] arr2 = {1, 2, 3, 4, 5, 6, 7};
+        int[] arr3 = {7, 6, 5, 4, 3, 2, 1};
+        int[] arr4 = {1, 6};
+        int[] arr5 = {6, 1};
+        System.out.println(p63.MaxDiff(arr1));
+        System.out.println(p63.MaxDiff(arr2));
+        System.out.println(p63.MaxDiff(arr3));
+        System.out.println(p63.MaxDiff(arr4));
+        System.out.println(p63.MaxDiff(arr5));
+    }
+}
