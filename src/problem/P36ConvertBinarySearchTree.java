7f89b61ee12ebc3bbbd2d0b4bddb8b2f8d53020f
Myth-AliCloud <caipengbo@outlook.com>
2020-02-09 18:30:02 +0800

    Review P36

--- src/problem/P36ConvertBinarySearchTree.java
+++ src/problem/P36ConvertBinarySearchTree.java
@@ -12,7 +12,7 @@ import java.io.IOException;
  * Created by Myth on 6/12/2019
  */
 public class P36ConvertBinarySearchTree {
-    // 多组测试数据不要使用static
+    // 多组测试数据不要使用 static
     private static TreeNode pLastNodeInList = null;
     public static TreeNode convert(TreeNode pRootOfTree) {
         convertNode(pRootOfTree);
@@ -23,7 +23,7 @@ public class P36ConvertBinarySearchTree {
         }
         return head;
     }
-    // 中序遍历的基础
+    // 以中序遍历为基础
     public static void convertNode(TreeNode current) {
         if (current == null) return;
         if (current.left != null)
@@ -38,16 +38,52 @@ public class P36ConvertBinarySearchTree {
             convertNode(current.right);
     }
 
+    // 第二遍
+    // left前指针  right后指针
+    public TreeNode convert2(TreeNode root) {
+        if (root == null) return null;
+        pre = null;
+        convertHelper(root);
+        TreeNode p = root;
+        while(p.left != null) p = p.left;
+        return p;
+    }
+    // 中序遍历 记录前一个访问的节点
+    // 注意 这样传pre的值是不会改变pre的
+    public void convertHelper(TreeNode cur, TreeNode pre) {
+        if (cur == null) return;
+        convertHelper(cur.left, pre);
+
+        cur.left = pre;
+        if (pre != null) pre.right = cur;
+        pre = cur;
+
+        convertHelper(cur.right, pre);
+    }
+    // 修改
+    TreeNode pre = null;
+    public void convertHelper(TreeNode cur) {
+        if (cur == null) return;
+        convertHelper(cur.left);
+
+        cur.left = pre;
+        if (pre != null) pre.right = cur;
+        pre = cur;
+
+        convertHelper(cur.right);
+    }
+
     public static void main(String[] args) throws IOException {
         int count = 0;
         String line;
         BufferedReader bufferedReader = Input.getBufferReader();
-
+        P36ConvertBinarySearchTree p36 = new P36ConvertBinarySearchTree();
         while ((line = bufferedReader.readLine()) != null) {
             count++;
             System.out.println("==========Test" + count + "=========================");
             TreeNode root = TreeUtil.stringToTreeNode(line);
-            TreeNode p = P36ConvertBinarySearchTree.convert(root);
+            TreeUtil.prettyPrintTree(root);
+            TreeNode p = p36.convert2(root);
             while (p.right != null) {
                 //System.out.println(p.val + " ");
                 p = p.right;
